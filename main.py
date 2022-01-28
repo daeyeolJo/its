4758ea69b1a711b44c7136e214ab10f5426f4874
Daeyeol Jo <41155899+dayeoljome@users.noreply.github.com>
2022-01-28 15:51:58 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ PATH = os.path.dirname(os.path.abspath(__file__))
 
 if __name__ == '__main__':
     fname = "test.xml"
-    cpu_model = input("1 : westmere, 2 : host-passthrough")
-    cpu_cache_mode = input("1 : disable, 2 : passthrough")
+
+    os_option = input("1 : gooroom, 2 : window10\n")
+    cpu_model = input("1 : westmere, 2 : host-passthrough\n")
+    cpu_cache_mode = input("1 : disable, 2 : passthrough\n")
 
     param = {'cpu_model' : cpu_model, 'cpu_cache_mode' : cpu_cache_mode }
 
@@ -16,12 +18,28 @@ if __name__ == '__main__':
 
     file_loader = FileSystemLoader('libvirt-xml')
     env = Environment(loader=file_loader)
-    template = env.get_template('gooroom.auto.xml')
+    if os_option == '1':
+        template = env.get_template('gooroom.auto.xml')
+        start_instruction = "virsh start gooroom"
+        shutdown_instruction = "virsh shutdown gooroom"
+    else:
+        template = env.get_template('win10.auto.xml')
+        start_instruction = "virsh start win10"
+        shutdown_instruction = "virsh shutdown win10"
     output_from_parsed_template = template.render(param=param)
-
-    define_instruction = "virsh define" + new_fname
-    os.system(define_instruction)
-
 
     with open(new_fname, 'w') as f:
         f.write(output_from_parsed_template)
+
+    define_instruction = "virsh define " + new_fname
+    os.system(define_instruction)
+
+    check_cpu_usage_instruction = "pidstat -G qemu-system-x86"
+    os.system(check_cpu_usage_instruction)
+
+    while(1):
+        user_input = input("If you want to shutdown VM, click q or Q")
+        if(user_input == 'q' or 'Q'):
+            os.system(shutdown_instruction)
+            break
+
