71f2f5b49af32ec3b83862383034fdd44eafb05a
Daeyeol Jo <41155899+dayeoljome@users.noreply.github.com>
2022-02-01 15:24:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,26 @@
-# import lxml import etree as ET
 import os, sys
 import datetime
 from jinja2 import Environment, FileSystemLoader
 PATH = os.path.dirname(os.path.abspath(__file__))
 
 if __name__ == '__main__':
-    fname = "test.xml"
-
+    fname = "gooroom"
     os_option = input("1 : gooroom, 2 : window10\n")
-    cpu_model = input("1 : westmere, 2 : host-passthrough\n")
-    cpu_cache_mode = input("1 : disable, 2 : passthrough\n")
-    disk_type = input("1 : ide, 2 : virtio")
-    disk_aio_mode = input("1 : native, 2 : queue")
-    video_model = input("1: cirrus, 2: virtio")
+    cpu_model = input("1 : host-passthrough, 2 : westmere\n")
+    cpu_cache_mode = input("1 : passthrough, 2 : disable\n")
+    disk_aio_mode = input("1 : threads, 2 : native, 3 : queue\n")
+    video_model = input("1 : vga, 2 : cirrus\n")
+    param = {'cpu_model' : cpu_model, 'cpu_cache_mode' : cpu_cache_mode, 'disk_aio_mode': disk_aio_mode, 'video_model' : video_model}
 
-    param = {'cpu_model' : cpu_model, 'cpu_cache_mode' : cpu_cache_mode, 'disk_type': disk_type,
-            'disk_aio_mode' : disk_aio_mode, 'video_model': video_model}
-
-    suffix = datetime.datetime.now().strftime("%y%m%d_%H%M%S")
-    new_fname = "_".join([fname, suffix]) + ".xml" # e.g. 'mylogfile_120508_171442'
-
+    suffix = datetime.datetime.now().strftime("%y%m%d_%S")
+    new_fname = "_".join([fname, suffix]) # e.g. 'mylogfile_120508_171442'
+    new_fname += ("_" + cpu_model + cpu_cache_mode + disk_aio_mode + video_model +".xml")
     file_loader = FileSystemLoader('libvirt-xml')
     env = Environment(loader=file_loader)
     if os_option == '1':
-        template = env.get_template('gooroom.auto.xml')
-        start_instruction = "virsh start gooroom"
-        shutdown_instruction = "virsh shutdown gooroom"
+        template = env.get_template('ubuntu.xml')
+        start_instruction = "virsh start udesktop20_04-03"
+        shutdown_instruction = "virsh shutdown udesktop20_04-03"
     else:
         template = env.get_template('win10.auto.xml')
         start_instruction = "virsh start win10"
@@ -37,8 +32,9 @@ if __name__ == '__main__':
 
     define_instruction = "virsh define " + new_fname
     os.system(define_instruction)
+    os.system(start_instruction)
 
-    check_cpu_usage_instruction = "pidstat -G qemu-system-x86"
+    check_cpu_usage_instruction = "pidstat -G qemu-kvm"
     os.system(check_cpu_usage_instruction)
 
     while(1):
@@ -46,4 +42,3 @@ if __name__ == '__main__':
         if(user_input == 'q' or 'Q'):
             os.system(shutdown_instruction)
             break
-
