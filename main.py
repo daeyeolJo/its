3af4dcea11ad202277c8dc07291d54466177ed6a
Daeyeol Jo <41155899+dayeoljome@users.noreply.github.com>
2022-02-03 13:17:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-import os, sys
+import os, sys, time, threading
 import datetime
 from jinja2 import Environment, FileSystemLoader
 PATH = os.path.dirname(os.path.abspath(__file__))
+
+def check_cpu_usage():
+    while(1):
+        check_cpu_usage_instruction = "pidstat -G qemu-system-x86"
+        os.system(check_cpu_usage_instruction)
+        time.sleep(5)
 
 if __name__ == '__main__':
     fname = "gooroom"
@@ -13,14 +19,14 @@ if __name__ == '__main__':
     param = {'cpu_model' : cpu_model, 'cpu_cache_mode' : cpu_cache_mode, 'disk_aio_mode': disk_aio_mode, 'video_model' : video_model}
 
     suffix = datetime.datetime.now().strftime("%y%m%d_%S")
-    new_fname = "_".join([fname, suffix]) # e.g. 'mylogfile_120508_171442'
+    new_fname = "_".join([fname, suffix]) 
     new_fname += ("_" + cpu_model + cpu_cache_mode + disk_aio_mode + video_model +".xml")
     file_loader = FileSystemLoader('libvirt-xml')
     env = Environment(loader=file_loader)
     if os_option == '1':
-        template = env.get_template('ubuntu.xml')
-        start_instruction = "virsh start udesktop20_04-03"
-        shutdown_instruction = "virsh shutdown udesktop20_04-03"
+        template = env.get_template('gooroom.auto.xml')
+        start_instruction = "virsh start gooroom"
+        shutdown_instruction = "virsh shutdown gooroom"
     else:
         template = env.get_template('win10.auto.xml')
         start_instruction = "virsh start win10"
@@ -34,11 +40,12 @@ if __name__ == '__main__':
     os.system(define_instruction)
     os.system(start_instruction)
 
-    check_cpu_usage_instruction = "pidstat -G qemu-kvm"
-    os.system(check_cpu_usage_instruction)
+    t = threading.Thread(target=check_cpu_usage)
+    t.daemon = True
+    t.start()
 
     while(1):
         user_input = input("If you want to shutdown VM, click q or Q")
-        if(user_input == 'q' or 'Q'):
+        if (user_input == 'q' or 'Q'):
             os.system(shutdown_instruction)
             break
